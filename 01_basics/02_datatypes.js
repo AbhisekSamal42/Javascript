fe3a7f78aee698c2f547091331e14a80c5c4709f
Abhisek Samal <samalabhisek42@gmail.com>
2025-07-30 17:22:48 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all the js code newer version
+
+// alert(3+3) ( if we are using nodejs it is showing error, but if we use broswer it show popup 6 ).
+
+let name = "Abhisek"
+let age = 23
+let isLogged = false
+
+// Primitive Datatype
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean
+null => standalone value (empty value).
+undefined => (variable is declared but the variable having no value).
+symbol => unique
+*/
+
+// Non primitive Datatype
+
+/*
+object
+
+*/
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // null is an object
+console.log(typeof undefined);
