14e781eec92d53db4ad5536fdd7145a179889b6e
Abhisek Samal <samalabhisek42@gmail.com>
2025-07-31 17:36:18 +0000

    Datatype Summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,53 @@
+// Primitive Datatype
+
+/*
+String
+Symbol
+Number
+Null
+Boolean
+BigInt
+Undefined
+*/
+
+
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+
+
+const bigNumber = 789652101458623n
+
+
+// Non-Primitive or Reference Datatype
+
+/*
+Array
+Object
+Function
+*/
+
+
+const hero = ['ram','hanuman','shiva']
+
+let myObj = {
+    name : 'Abhisek',
+    age : 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hii Everyone");
+}
