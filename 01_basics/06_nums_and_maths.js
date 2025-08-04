b3323ad94eb2e819f0e7ed748269fb79f2cd2bbf
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-04 17:28:22 +0000

    Number and Math methods

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400;
+console.log(score)
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3))
+
+
+const hunders = 1000000
+console.log(hunders.toLocaleString('en-IN'))
+
+
+// ***************** Maths ****************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.round(4.8));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(3, 4, 56, 7));
+console.log(Math.max(1, 23, 54, 78));
+ 
+console.log(Math.random()); 
+console.log((Math.random() * 10) + 1);
+// Value of random always give in between 0 and 1.
+
+
+const min = 10
+const max = 20
+
+console.log (Math.floor(Math.random()*(max-min + 1))+min)
