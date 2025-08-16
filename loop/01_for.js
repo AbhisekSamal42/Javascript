1c2788db39ddd0881390f475417c63966ed326da
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-16 05:22:12 +0000

    For Loop

---
+++ loop/01_for.js
@@ -0,0 +1,11 @@
+for (let count =1 ; count <= 5 ; count++){
+    console.log("Hii Abhisek");
+    
+}
+
+// Calculate sum of 1 to n number .
+let sum = 0;
+for (let i = 1; i <= 20; i++){
+    sum = sum + i;
+}
+console.log(sum);
