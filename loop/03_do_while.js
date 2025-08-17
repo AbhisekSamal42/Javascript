7aec94509aa0bad4044f162deffa0262c5701847
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-17 13:44:25 +0000

    do-while Loop

---
+++ loop/03_do_while.js
@@ -0,0 +1,16 @@
+let i = 1;
+do{
+    console.log("Hii Abhisek");
+    i++;
+}while(i<=5);
+
+
+// Calculate sum of 1 to n number.
+
+let sum = 0;
+let j = 1 ;
+do{
+    sum = sum + j;
+    j++;
+}while(j<=100);
+console.log(sum);
