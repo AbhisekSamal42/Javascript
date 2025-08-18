8b9d55308d16ea478c9378e3c11d169623abfcbe
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-18 05:14:08 +0000

    For-in Loop

---
+++ loop/05_for_in.js
@@ -0,0 +1,10 @@
+let stu ={
+    name : "Abhisek Samal",
+    age : 20,
+    cgpa : 7.8,
+    isPass : true,
+}
+for (let i in stu){
+    console.log("key=",i);
+    console.log("key=",i, "value=",stu[i]);
+}
