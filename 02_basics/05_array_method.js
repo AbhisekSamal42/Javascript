0b7d65d28b86ca330bf74f3ee4905994ef7580c2
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-21 06:33:54 +0000

    ForEach method

---
+++ 02_basics/05_array_method.js
@@ -0,0 +1,9 @@
+/* -> It is an array method . It run a function once 
+for every element in the array .
+-> It does not return new array.
+ */
+
+let fruits = ['Abhisek', 'Banana', 'Mango'];
+fruits.forEach(function(item){
+    console.log(item);
+});
