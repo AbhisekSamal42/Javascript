2e9f5db729db7232e93a3a1efb88b58d81922516
Abhisek Samal <samalabhisek42@gmail.com>
2025-08-22 05:58:26 +0000

    For Each method

--- 02_basics/05_array_method.js
+++ 02_basics/05_array_method.js
@@ -7,3 +7,11 @@ let fruits = ['Abhisek', 'Banana', 'Mango'];
 fruits.forEach(function(item){
     console.log(item);
 });
+
+// For a given array of numbers, print the square of each value using the forEach loop.
+
+let num = [1,2,3,4,5,6]
+
+num.forEach(function(num){
+    console.log(num*num);
+});
